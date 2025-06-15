45d02584966683631869ed3cd6926b7a4328c3f8
l <eh.chahkiev@smartics.ru>
2025-06-16 01:20:01 +0300

    feat: add hono client

---
+++ index.ts
@@ -0,0 +1,3 @@
+import { hc as honoClient } from 'hono/client'
+
+export const hc = honoClient('http://localhost:3000')
--- main.ts
+++ main.ts
@@ -6,12 +6,18 @@ const rooms = new Map()
 const app = new Hono()
   .get('/', (c) => c.html('<h1>server is ok</h1>'))
   .get('/ws/rooms/:roomId', upgradeWebSocket(context => {
+    const roomId = context.req.param('roomId')
     return {
       onOpen(event, ws) {
-
+        rooms.set(roomId, ws)
       },
-      onMessage() {
+      onMessage(event, ws) {
+        console.info(event)
+        rooms.get(roomId)
         console.log('Message received')
+      },
+      onClose(event, ws) {
+        rooms.delete(roomId)
       }
     }
   })
