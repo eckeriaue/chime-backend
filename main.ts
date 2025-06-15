6ad8e4b827e22cf891f61b172d14e089375b005a
l <eh.chahkiev@smartics.ru>
2025-06-16 01:00:27 +0300

    feat: health check

--- main.ts
+++ main.ts
@@ -1,13 +1,20 @@
 import { Hono } from 'hono'
 import { upgradeWebSocket } from 'hono/deno'
 
+const rooms = new Map()
 
-const app = new Hono().get('/ws/rooms/:roomId', upgradeWebSocket(context => {
-  return {
-    onMessage() {
-      console.log('Message received')
+const app = new Hono()
+  .get('/', (c) => c.html('<h1>server is ok</h1>'))
+  .get('/ws/rooms/:roomId', upgradeWebSocket(context => {
+    return {
+      onOpen(event, ws) {
+
+      },
+      onMessage() {
+        console.log('Message received')
+      }
     }
-  }
-}))
+  })
+  )
 
 Deno.serve({ port: 3000 }, app.fetch)
